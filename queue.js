1361ed1f73dc7ad01f3604b6a40cca9511a445ef
annika <annikaslund@gmail.com>
2019-04-08 15:16:28 -0700

    Add enqueue function to Queue

--- queue.js
+++ queue.js
@@ -20,7 +20,21 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    // end of queue is head
+    // if size is 0, first and last are newNode
+    // else, last.next is newNode
+    // newNode is this.last
+    let newNode = new Node(val);
 
+    if (this.size === 0){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.size += 1;
   }
 
   /** dequeue(): remove the node from the start of the queue

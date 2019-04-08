71a6209ba8ca89ee84f7477bc99506167281a36d
annika <annikaslund@gmail.com>
2019-04-08 15:31:09 -0700

    Add peek function to Stack

--- stack.js
+++ stack.js
@@ -45,7 +45,7 @@ class Stack {
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
